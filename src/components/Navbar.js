0ce9e9b0960a4cb015dced0b21ece174e9904bca
gilbert866 <amo_gilbert@yahoo.com>
2024-05-27 15:23:40 +0000

    Updated commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -92,7 +92,7 @@ function NavBar() {
             {/* Link to Portfolio Github */}
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/gilbert866/my_portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
